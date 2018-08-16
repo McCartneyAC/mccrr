1870fef0912cf0a716ed3d15fe8cf023c3eca64c
McCartneyAC <acm9q@virginia.edu>
2018-08-16 18:55:26 -0400

    Update RDD.R

--- R/RDD.R
+++ R/RDD.R
@@ -1,6 +1,6 @@
 # tools and functions for regression discontinuity designs
 
-
+# These may become a part of a package of tools associated with Dr. Miller's Causal Inference class at UVA
 
 center_rv <- function(df, var, center = 0) {
   # centers a running variable on user input center
