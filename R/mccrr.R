060bede4feaeb377c025e8d19d68c1f44bd84f86
McCartneyAC <acm9q@virginia.edu>
2018-09-20 20:37:03 -0400

    Update mccrr.R

--- R/mccrr.R
+++ R/mccrr.R
@@ -180,3 +180,15 @@ regress <- function(df, ...) {
     lm(data = df, ...)
   )
 }
+
+# valentine's day
+heart<-function(){
+  dat<- data.frame(t=seq(0, 2*pi, by=0.1) ) 
+  xhrt <- function(t) 16*sin(t)^3 
+  yhrt <- function(t) 13*cos(t)-5*cos(2*t)-2*cos(3*t)-cos(4*t) 
+  dat$y =yhrt(dat$t) 
+  dat$x=xhrt(dat$t) 
+  plot(y ~ x, data=dat, type="l", bty="n", xaxt="n", yaxt="n", ann=FALSE) 
+  with(dat, polygon(x,y, col="hotpink")) 
+  points(c(10,-10, -15, 15), c(-10, -10, 10, 10), pch=169, font=5)
+}
