3620e6c15980ddc412ab21d4943f52469708fe5e
Andrew McCartney <acm9q@virginia.edu>
2019-03-28 14:27:25 -0400

    Update mccrr.R

--- R/mccrr.R
+++ R/mccrr.R
@@ -1,3 +1,18 @@
+#tmp <- capture.output(stem(iris$Petal.Length))
+#stemdf = data.frame(tmp, rr=1:length(tmp))
+#ggplot(stemdf)+ geom_text(aes(x=rr, y=0, label=tmp), hjust=0) +
+#    coord_flip()+ theme_classic() +
+#    scale_x_discrete(breaks=NULL)+
+#    scale_y_discrete(breaks=NULL, limits=c(0,1))+
+#    theme(axis.text = element_blank(),
+#        axis.title = element_blank(),
+#        axis.ticks=element_blank(),
+#        panel.grid=element_blank(),
+#        axis.line=element_blank())
+#https://stackoverflow.com/questions/26532564/how-to-output-a-stem-and-leaf-plot-as-a-plot
+
+
+
 #' Vlookup a value
 #'
 #' from Jenny Bryan @JennyBryan
